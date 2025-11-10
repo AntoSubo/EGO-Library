0bb64575d0920a64d233718ef26ed4aafe8bf611
semyanovame <semyanovame@309D-13>
2025-11-10 11:29:57 +0700

    2.0

--- EGO Library/Models/EgoGift.cs
+++ EGO Library/Models/EgoGift.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace EGO_Library.Models
 {
@@ -16,5 +19,21 @@ namespace EGO_Library.Models
         public string Description { get; set; } 
         public List<string> Sources { get; set; }
         public List<string> FusionRecipes { get; set; }
+
+        public EgoGift() { }
+
+        
+        public EgoGift(string id, string name, int tier, string status, string description)
+        {
+            Id = id;
+            Name = name;
+            Tier = tier;
+            StatusEffect = status;
+            Description = description;
+            Sources = new List<string>();
+            FusionRecipes = new List<string>();
+        }
+
     }
+
 }
