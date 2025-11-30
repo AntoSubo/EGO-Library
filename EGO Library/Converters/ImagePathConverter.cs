95a48da1a4f4d7908b022a5d8552977999efe54c
rimoondust <suboannto@gmail.com>
2025-11-30 18:28:03 +0700

    30.11.25

---
+++ EGO Library/Converters/ImagePathConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace EGO_Library.Converters
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return null;
+
+            string imagePath = value.ToString();
+
+            try
+            {
+                // Если путь уже в формате pack://, используем как есть
+                if (imagePath.StartsWith("pack://"))
+                {
+                    return new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+                }
+
+                // Если это относительный путь
+                if (File.Exists(imagePath))
+                {
+                    return new BitmapImage(new Uri(Path.GetFullPath(imagePath)));
+                }
+
+                // Пробуем найти в ресурсах
+                string resourcePath = $"pack://application:,,,/{imagePath.TrimStart('/')}";
+                return new BitmapImage(new Uri(resourcePath, UriKind.Absolute));
+            }
+            catch
+            {
+                // Если изображение не найдено, возвращаем null (будет черный квадрат)
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
